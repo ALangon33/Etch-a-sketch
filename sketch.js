0528787ddfb0c874ab7599f896e283ebcac76b57
alangon03 <alangon03@gmail.com>
2023-06-25 16:26:20 -0700

    Add click and drag functionality

--- sketch.js
+++ sketch.js
@@ -12,34 +12,46 @@ bodyContainer.appendChild(canvasContainer);
 
 let gridSize = 16;
 
-for (i=0;i<gridSize;i++) {
-    gridRowDiv = document.createElement('div');
-    gridRowDiv.id = 'grid-row'
-    gridRowDiv.style.flex = '1'
-    gridRowDiv.style.flexFlow = 'row nowrap'
-    canvasContainer.appendChild(gridRowDiv);
-    for (n=0;n<gridSize;n++){
-          gridColumnDiv = document.createElement('div');
-          gridColumnDiv.style.width = `${(960/gridSize) + 'px'}`
-          gridColumnDiv.style.height = `${(960/gridSize) + 'px'}`
-          gridColumnDiv.style.flex = '1'
-          gridColumnDiv.id = 'grid-tile';
-        gridColumnDiv.style.flexFlow = 'column nowrap'
-        gridRowDiv.appendChild(gridColumnDiv);
-    }; 
+for (i = 0; i < gridSize; i++) {
+  gridRowDiv = document.createElement('div');
+  gridRowDiv.id = 'grid-row'
+  gridRowDiv.style.flex = '1'
+  gridRowDiv.style.flexFlow = 'row nowrap'
+  canvasContainer.appendChild(gridRowDiv);
+  for (n = 0; n < gridSize; n++) {
+    gridColumnDiv = document.createElement('div');
+    gridColumnDiv.style.width = `${(960 / gridSize) + 'px'}`
+    gridColumnDiv.style.height = `${(960 / gridSize) + 'px'}`
+    gridColumnDiv.style.flex = '1'
+    gridColumnDiv.id = 'grid-tile';
+    gridColumnDiv.style.flexFlow = 'column nowrap'
+    gridRowDiv.appendChild(gridColumnDiv);
+  };
 };
+
+let isDown = false;
 
 const gridRows = document.querySelectorAll('#grid-row')
 const gridTiles = document.querySelectorAll('#grid-tile')
 
-gridTiles.forEach(gridTile => gridTile.addEventListener('mouseover', changeColor, {
-    capture: false
+gridTiles.forEach(gridTile => gridTile.addEventListener('mousedown', () => {
+  isDown = true;
+  gridTile.classList.add('blackground');
+  gridRowDiv.appendChild(gridColumnDiv);
   }));
-  
 
-function changeColor(e) {
-    e = this;
-    console.log(e);
-    this.style.backgroundColor = 'black';
+gridTiles.forEach(gridTile => gridTile.addEventListener('mousemove', () => {
+  if (isDown) {
+    gridTile.classList.add('blackground');
     gridRowDiv.appendChild(gridColumnDiv);
+  };
+}));
+
+gridTiles.forEach(gridTile => gridTile.addEventListener('mouseup', () => {
+  isDown = false;
+}));
+
+
+function changeColor() {
+
 }
