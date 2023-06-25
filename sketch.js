2d97e13b441943662bae04ccfc89e4d6b0bed260
alangon03 <alangon03@gmail.com>
2023-06-25 15:02:15 -0700

    Get event listener to change background color on a per tile basis

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 const bodyContainer = document.querySelector('body');
 const canvasContainer = document.createElement('div');
+
 canvasContainer.id = 'canvas-container';
 canvasContainer.style.width = '960px';
 canvasContainer.style.height = '960px';
@@ -13,6 +14,7 @@ let gridSize = 16;
 
 for (i=0;i<gridSize;i++) {
     gridRowDiv = document.createElement('div');
+    gridRowDiv.id = 'grid-row'
     gridRowDiv.style.flex = '1'
     gridRowDiv.style.flexFlow = 'row nowrap'
     canvasContainer.appendChild(gridRowDiv);
@@ -21,13 +23,23 @@ for (i=0;i<gridSize;i++) {
           gridColumnDiv.style.width = `${(960/gridSize) + 'px'}`
           gridColumnDiv.style.height = `${(960/gridSize) + 'px'}`
           gridColumnDiv.style.flex = '1'
-          gridColumnDiv.id = 'gridTile';
+          gridColumnDiv.id = 'grid-tile';
         gridColumnDiv.style.flexFlow = 'column nowrap'
         gridRowDiv.appendChild(gridColumnDiv);
     }; 
 };
 
-function changeColor(a,b) {
-    a.style.backgroundColor = 'black';
-    b.appendChild(a);
+const gridRows = document.querySelectorAll('#grid-row')
+const gridTiles = document.querySelectorAll('#grid-tile')
+
+gridTiles.forEach(gridTile => gridTile.addEventListener('mouseover', changeColor, {
+    capture: false
+  }));
+  
+
+function changeColor(e) {
+    e = this;
+    console.log(e);
+    this.style.backgroundColor = 'black';
+    gridRowDiv.appendChild(gridColumnDiv);
 }
