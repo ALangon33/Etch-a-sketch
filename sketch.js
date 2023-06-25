16db2e447321f414661d2bc7dd918cb03bee18e9
alangon03 <alangon03@gmail.com>
2023-06-25 14:43:14 -0700

    Fix canvas so it's actually a grid

--- sketch.js
+++ sketch.js
@@ -5,24 +5,29 @@ canvasContainer.style.width = '960px';
 canvasContainer.style.height = '960px';
 canvasContainer.style.backgroundColor = 'white';
 canvasContainer.style.display = 'flex';
-canvasContainer.style.flexFlow = 'column nowrap';
+canvasContainer.style.flexFlow = 'row nowrap';
 canvasContainer.style.alignItems = 'center';
 bodyContainer.appendChild(canvasContainer);
 
 let gridSize = 16;
 
 for (i=0;i<gridSize;i++) {
-    let gridRowDiv = document.createElement('div');
+    gridRowDiv = document.createElement('div');
     gridRowDiv.style.flex = '1'
     gridRowDiv.style.flexFlow = 'row nowrap'
     canvasContainer.appendChild(gridRowDiv);
     for (n=0;n<gridSize;n++){
-          let gridColumnDiv = document.createElement('div');
+          gridColumnDiv = document.createElement('div');
           gridColumnDiv.style.width = `${(960/gridSize) + 'px'}`
           gridColumnDiv.style.height = `${(960/gridSize) + 'px'}`
           gridColumnDiv.style.flex = '1'
+          gridColumnDiv.id = 'gridTile';
         gridColumnDiv.style.flexFlow = 'column nowrap'
         gridRowDiv.appendChild(gridColumnDiv);
-    };    
+    }; 
 };
 
+function changeColor(a,b) {
+    a.style.backgroundColor = 'black';
+    b.appendChild(a);
+}
