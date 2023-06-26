d387d9ae7de5a9944789cb4aabecacfb1d1fb951
alangon03 <alangon03@gmail.com>
2023-06-25 17:33:36 -0700

    Fix canvas replication issue

--- sketch.js
+++ sketch.js
@@ -1,57 +1,78 @@
 const bodyContainer = document.querySelector('body');
 const canvasContainer = document.createElement('div');
-
-canvasContainer.id = 'canvas-container';
-canvasContainer.style.width = '960px';
-canvasContainer.style.height = '960px';
-canvasContainer.style.backgroundColor = 'white';
-canvasContainer.style.display = 'flex';
-canvasContainer.style.flexFlow = 'row nowrap';
-canvasContainer.style.alignItems = 'center';
-bodyContainer.appendChild(canvasContainer);
-
+const btn = document.querySelector('button');
+const canvasSize = 960;
 let gridSize = 16;
+  generateCanvas();
+  generateGrid(gridSize);
 
-for (i = 0; i < gridSize; i++) {
-  gridRowDiv = document.createElement('div');
-  gridRowDiv.id = 'grid-row'
-  gridRowDiv.style.flex = '1'
-  gridRowDiv.style.flexFlow = 'row nowrap'
-  canvasContainer.appendChild(gridRowDiv);
-  for (n = 0; n < gridSize; n++) {
-    gridColumnDiv = document.createElement('div');
-    gridColumnDiv.style.width = `${(960 / gridSize) + 'px'}`
-    gridColumnDiv.style.height = `${(960 / gridSize) + 'px'}`
-    gridColumnDiv.style.flex = '1'
-    gridColumnDiv.id = 'grid-tile';
-    gridColumnDiv.style.flexFlow = 'column nowrap'
-    gridRowDiv.appendChild(gridColumnDiv);
+function generateCanvas() {
+  canvasContainer.id = 'canvas-container';
+  canvasContainer.style.width = `${canvasSize + 'px'}`;
+  canvasContainer.style.height = `${canvasSize + 'px'}`;
+  canvasContainer.style.backgroundColor = 'white';
+  canvasContainer.style.display = 'flex';
+  canvasContainer.style.flexFlow = 'row nowrap';
+  canvasContainer.style.alignItems = 'center';
+  bodyContainer.appendChild(canvasContainer);
+};
+
+function generateGrid(a) {
+  for (i = 0; i < a; i++) {
+    gridRowDiv = document.createElement('div');
+    gridRowDiv.id = 'grid-row';
+    gridRowDiv.style.flex = '1';
+    gridRowDiv.style.flexFlow = 'row nowrap';
+    canvasContainer.appendChild(gridRowDiv);
+
+    for (n = 0; n < a; n++) {
+      gridColumnDiv = document.createElement('div');
+      gridColumnDiv.style.width = `${(960 / a) + 'px'}`;
+      gridColumnDiv.style.height = `${(960 / a) + 'px'}`;
+      gridColumnDiv.style.flex = '1';
+      gridColumnDiv.id = 'grid-tile';
+      gridColumnDiv.style.flexFlow = 'column nowrap';
+      gridRowDiv.appendChild(gridColumnDiv);
+    };
   };
+
+  let gridTiles = document.querySelectorAll('#grid-tile');
+  let gridRows = document.querySelectorAll('#grid-rows');
+
+  gridTiles.forEach(gridTile => gridTile.addEventListener('mousedown', () => {
+    isDown = true;
+    gridTile.classList.add('blackground');
+    gridRowDiv.appendChild(gridColumnDiv);
+  }));
+  
+  gridTiles.forEach(gridTile => gridTile.addEventListener('mousemove', () => {
+    if (isDown) {
+      gridTile.classList.add('blackground');
+      gridRowDiv.appendChild(gridColumnDiv);
+    };
+  }));
+  
+  gridTiles.forEach(gridTile => gridTile.addEventListener('mouseup', () => {
+    isDown = false;
+  }));
+  
+  canvasContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+  });
 };
 
 let isDown = false;
 
-const gridRows = document.querySelectorAll('#grid-row')
-const gridTiles = document.querySelectorAll('#grid-tile')
+let gridTiles = document.querySelectorAll('#grid-tile');
+let gridRows = document.querySelectorAll('#grid-row');
 
-gridTiles.forEach(gridTile => gridTile.addEventListener('mousedown', () => {
-  isDown = true;
-  gridTile.classList.add('blackground');
-  gridRowDiv.appendChild(gridColumnDiv);
-  }));
+btn.style.margin = '30px';
+btn.style.padding = '10px';
 
-gridTiles.forEach(gridTile => gridTile.addEventListener('mousemove', () => {
-  if (isDown) {
-    gridTile.classList.add('blackground');
-    gridRowDiv.appendChild(gridColumnDiv);
-  };
-}));
-
-gridTiles.forEach(gridTile => gridTile.addEventListener('mouseup', () => {
-  isDown = false;
-}));
+btn.addEventListener('click', () => {
+  canvasContainer.innerHTML = '';
+  gridSize = prompt("# x # Grid : ")
+  generateGrid(gridSize);
+});
 
 
-function changeColor() {
-
-}
